7baaaa9cdeba7a534a9bce923ff24b88a108df3c
juhee kang <mnmn092631@gmail.com>
2024-03-08 16:34:39 +0900

    12. Node.js의 파일 읽기 기능

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
-const app = http.createServer(function (request, response) {
+const app = http.createServer((request, response) => {
   let _url = request.url;
   const queryData = url.parse(_url, true).query;
   let title = queryData.id;
