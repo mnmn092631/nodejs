f6ca2f44dca790785c77ecc47687dac31fb44d81
juhee kang <mnmn092631@gmail.com>
2024-03-12 00:00:37 +0900

    28.2. Node.js에서 동기와 비동기 2

---
+++ nodejs/sync.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+// readFileSync
+console.log("A");
+const result = fs.readFileSync("sample.txt", "utf8");
+console.log(result);
+console.log("C");
+
+// readFile
+console.log("A");
+fs.readFile("sample.txt", "utf8", (err, result) => {
+  console.log(result);
+});
+console.log("C");
