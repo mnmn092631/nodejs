8b72c004719f26a3191dbcbdc50bdfd51d4f28cf
juhee kang <mnmn092631@gmail.com>
2024-03-12 02:14:59 +0900

    35. 글 수정 - 수정할 정보 전송

--- main.js
+++ main.js
@@ -78,7 +78,7 @@ const app = http.createServer((request, response) => {
         title,
         list,
         `
-        <form action="http://localhost:3000/create_process" method="post">
+        <form action="/create_process" method="post">
         <p><input type="text" name="title" placeholder="title" /></p>
         <p><textarea name="description" placeholder="description" ></textarea></p>
         <p><input type="submit" /></p>
@@ -101,6 +101,28 @@ const app = http.createServer((request, response) => {
       fs.writeFile(`data/${title}`, description, "utf8", (err) => {
         response.writeHead(302, { Location: `/?id=${title}` });
         response.end();
+      });
+    });
+  } else if (pathname === "/update") {
+    fs.readdir("./data", (err, files) => {
+      fs.readFile(`data/${queryData.id}`, "utf8", (err, description) => {
+        const title = queryData.id;
+        const list = templateList(files);
+        const template = templateHTML(
+          title,
+          list,
+          `
+          <form action="/update_process" method="post">
+          <input type="hidden" name="id" value="${title}" />
+          <p><input type="text" name="title" placeholder="title" value="${title}" /></p>
+          <p><textarea name="description" placeholder="description">${description}</textarea></p>
+          <p><input type="submit" /></p>
+          </form>
+          `,
+          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`,
+        );
+        response.writeHead(200);
+        response.end(template);
       });
     });
   } else {
