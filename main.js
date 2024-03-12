5acec4ee3b98c95ab46b8af4d5604f3d2566cffe
juhee kang <mnmn092631@gmail.com>
2024-03-13 01:17:53 +0900

    45. 모듈의 활용

--- main.js
+++ main.js
@@ -2,36 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 const qs = require("querystring");
-
-const template = {
-  html: function (title, list, body, control) {
-    return `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          ${list}
-          ${control}
-          ${body}
-        </body>
-        </html>
-        `;
-  },
-  list: function (files) {
-    let list = "<ul>";
-    let i = 0;
-    while (i < files.length) {
-      list += `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`;
-      i++;
-    }
-    list += "</ul>";
-    return list;
-  },
-};
+const template = require("./lib/template.js");
 
 const app = http.createServer((request, response) => {
   let _url = request.url;
