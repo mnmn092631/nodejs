ba30252e6153ad98d83c2e5f588348196023b6ee
juhee kang <mnmn092631@gmail.com>
2024-03-13 00:55:57 +0900

    37. 글 삭제 - 삭제 버튼 구현

--- main.js
+++ main.js
@@ -62,7 +62,12 @@ const app = http.createServer((request, response) => {
             title,
             list,
             `<h2>${title}</h2><p>${description}</p>`,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`,
+            `<a href="/create">create</a>
+                    <a href="/update?id=${title}">update</a>
+                    <form action="/delete_process" method="post">
+                        <input type="hidden" name="id" value="${title}" />
+                        <input type="submit" value="delete" />   
+                    </form>`,
           );
           response.writeHead(200);
           response.end(template);
